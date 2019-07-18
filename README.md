734e9e4551857ca5c1e30852b639a36b988661a6
Michiel Borkent <michielborkent@gmail.com>
2019-07-18 21:28:18 +0200

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # jet
-JSON 2 EDN 2 Transit
+
+Transform JSON into EDN into Transit.
+
+## Usage
+
+`jet` supports three options:
+
+   `--from`: allowed values: `edn`, `transit` or `json`
+   `--to`: allowed values: `edn`, `transit` or `json`
+   `--keywordize`: allowed values: `true` or `false`
+   `--version`: prints current version of `jet`
+
+Examples:
+
+``` shellsession
+$ echo '{"a": 1}' | jet --from json --to edn --keywordize false
+{a 1}
+$ echo '{"a": 1}' | jet --from json --to edn --keywordize true
+{:a 1}
+$ echo '{"a": 1}' | jet --from json --to transit
+["^ ","a",1]
+```
+
+## Installation
+
+Install via brew:
+
+    brew install borkdude/brew/jet
+
+To upgrade:
+
+    brew upgrade jet
+
+## License
+
+Copyright © 2019 Michiel Borkent
+
+Distributed under the EPL License, same as Clojure. See LICENSE.
---
+++ project.clj
@@ -0,0 +1,25 @@
+(defproject jet #=(clojure.string/trim
+                   #=(slurp "resources/JET_VERSION"))
+  :description "jet"
+  :url "https://github.com/borkdude/jet"
+  :scm {:name "git"
+        :url "https://github.com/borkdude/jet"}
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"}
+  :source-paths ["src"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.cognitect/transit-clj "0.8.313"]
+                 [cheshire "5.8.1"]]
+  :profiles {:clojure-1.9.0 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :clojure-1.10.1 {:dependencies [[org.clojure/clojure "1.10.1"]]}
+             :test {:dependencies [[clj-commons/conch "0.9.2"]]}
+             :uberjar {:global-vars {*assert* false}
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"
+                                  "-Dclojure.spec.skip-macros=true"]
+                       :main jet.core
+                       :aot :all}}
+  :aliases {"jet" ["run" "-m" "jet.core"]}
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
