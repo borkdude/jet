d82edae9575bb15a9e36eba56399209e2f03d4ff
Michiel Borkent <michielborkent@gmail.com>
2020-02-02 09:44:33 +0100

    Windows build

---
+++ script/compile.bat
@@ -0,0 +1,37 @@
+@echo off
+
+Rem set GRAALVM_HOME=C:\Users\IEUser\Downloads\graalvm\graalvm-ce-19.2.1
+Rem set PATH=%PATH%;C:\Users\IEUser\bin
+
+if "%GRAALVM_HOME%"=="" (
+    echo Please set GRAALVM_HOME
+    exit /b
+)
+set JAVA_HOME=%GRAALVM_HOME%\bin
+set PATH=%PATH%;%GRAALVM_HOME%\bin
+
+set /P JET_VERSION=< resources\JET_VERSION
+echo Building jet %JET_VERSION%
+
+call lein with-profiles +clojure-1.10.1 do clean, uberjar
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+Rem the --no-server option is not supported in GraalVM Windows.
+call %GRAALVM_HOME%\bin\native-image.cmd ^
+  "-jar" "target/jet-%JET_VERSION%-standalone.jar" ^
+  "-H:Name=jet" ^
+  "-H:+ReportExceptionStackTraces" ^
+  "-J-Dclojure.spec.skip-macros=true" ^
+  "-J-Dclojure.compiler.direct-linking=true" ^
+  "-H:IncludeResources=JET_VERSION" ^
+  "-H:ReflectionConfigurationFiles=reflection.json" ^
+  "--initialize-at-build-time"  ^
+  "-H:Log=registerResource:" ^
+  "--no-fallback" ^
+  "--verbose" ^
+  "-J-Xmx3g"
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Creating zip archive
+jar -cMf jet-%JET_VERSION%-windows-amd64.zip jet.exe
