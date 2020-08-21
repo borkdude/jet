6a0a7d1c2ba78b2309b508b90cee3a9ceb0c7b81
Michiel Borkent <michielborkent@gmail.com>
2020-08-22 00:02:50 +0200

    Upgrade sci

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [cheshire "5.9.0"]
                  [fipp "0.6.22"]
                  [commons-io/commons-io "2.6"]
-                 [borkdude/sci "0.0.13-alpha.10"]]
+                 [borkdude/sci "0.1.0"]]
   :profiles {:clojure-1.9.0 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :clojure-1.10.2-alpha1 {:dependencies [[org.clojure/clojure "1.10.2-alpha1"]]}
              :test {:dependencies [[clj-commons/conch "0.9.2"]]}
