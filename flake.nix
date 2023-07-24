77a01d4de2eca0613f73feb7a1867a7271220f78
Jim Clark <slimslenderslacks@gmail.com>
2023-07-24 15:28:16 -0700

    fix clj-nix flake ref

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     clj-nix = {
-      url = "/Users/slim/slimslenderslacks/clj-nix";
+      url = "github:jlesquembre/clj-nix";
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
